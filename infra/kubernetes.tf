11de2f34b17612afbf738e006e4cdcf67e999bbc
wiktorkowalski <wiktor9196667@gmail.com>
2023-09-02 13:03:14 +0200

    infra: change eks to module

--- infra/kubernetes.tf
+++ infra/kubernetes.tf
@@ -1,91 +1,21 @@
 provider "kubernetes" {
-  host                   = aws_eks_cluster.playground.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.playground.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.playground.token
 }
 
 data "aws_eks_cluster_auth" "playground" {
-  name = aws_eks_cluster.playground.name
+  name = module.eks.cluster_name
 }
 
-# resource "kubernetes_namespace" "kubernetes_dashboard" {
-#   metadata {
-#     name = "kubernetes-dashboard"
-#   }
-# }
+module "kubernetes_dashboard" {
+  source = "cookielab/dashboard/kubernetes"
+  version = "0.9.0"
 
-# resource "kubernetes_service_account" "kubernetes_dashboard" {
-#   metadata {
-#     name      = "kubernetes-dashboard"
-#     namespace = kubernetes_namespace.kubernetes_dashboard.metadata[0].name
-#   }
-# }
+  kubernetes_namespace_create = true
+  kubernetes_dashboard_csrf = "214bbf08-61eb-44b9-b573-a756dbc1fc7d"
+}
 
-# resource "kubernetes_cluster_role_binding" "kubernetes_dashboard" {
-#   metadata {
-#     name = "kubernetes-dashboard"
-#   }
-#   role_ref {
-#     api_group = "rbac.authorization.k8s.io"
-#     kind      = "ClusterRole"
-#     name      = "cluster-admin"
-#   }
-#   subject {
-#     kind      = "ServiceAccount"
-#     name      = kubernetes_service_account.kubernetes_dashboard.metadata[0].name
-#     namespace = kubernetes_namespace.kubernetes_dashboard.metadata[0].name
-#   }
-# }
-
-# resource "kubernetes_deployment" "kubernetes_dashboard" {
-#   metadata {
-#     name      = "kubernetes-dashboard"
-#     namespace = kubernetes_namespace.kubernetes_dashboard.metadata[0].name
-#   }
-#   spec {
-#     replicas = 1
-#     selector {
-#       match_labels = {
-#         k8s-app = "kubernetes-dashboard"
-#       }
-#     }
-#     template {
-#       metadata {
-#         labels = {
-#           k8s-app = "kubernetes-dashboard"
-#         }
-#       }
-#       spec {
-#         container {
-#           image = "kubernetesui/dashboard:v2.7.0"
-#           name  = "kubernetes-dashboard"
-#           args  = ["--auto-generate-certificates", "--namespace=kubernetes-dashboard"]
-#         }
-#         service_account_name = kubernetes_service_account.kubernetes_dashboard.metadata[0].name
-#       }
-#     }
-#   }
-# }
-
-# resource "kubernetes_service" "kubernetes_dashboard" {
-#   metadata {
-#     name      = "kubernetes-dashboard"
-#     namespace = kubernetes_namespace.kubernetes_dashboard.metadata[0].name
-#     labels = {
-#       k8s-app = "kubernetes-dashboard"
-#     }
-#   }
-#   spec {
-#     selector = {
-#       k8s-app = "kubernetes-dashboard"
-#     }
-#     port {
-#       port        = 443
-#       target_port = 8443
-#     }
-#   }
-# }
-
-# output "kubernetes_dashboard_url" {
-#   value = "${aws_eks_cluster.playground.endpoint}/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
-# }
+output "dashboard_url" {
+  value = module.kubernetes_dashboard.dashboard_url
+}
